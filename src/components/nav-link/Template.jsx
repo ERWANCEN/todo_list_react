8d3d800ebab84ea017f5b84f97865782e0ce5c92
ERWANCEN <erwance@hotmail.fr>
2026-01-11 19:31:35 +0100

    Adding task

--- src/components/nav-link/Template.jsx
+++ src/components/nav-link/Template.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Outlet } from 'react-router';
 
 import Header from './Header';
-import Footer from './Footer';
 
 const Template = () => {
   return (
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,16 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { TODO_FIELDS } from './TodoFields';
 
 const Todo = () => {
     const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    const handleChange = (event) => {
+        setNewTask(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (newTask.trim() === "") return;
+
+        setTasks(prevTasks => [...prevTasks, newTask]);
+        setNewTask("");
+    }
 
     return (
         <>
-            <h1>ToDo List</h1>
-            {tasks.length > 0 && tasks.map((task, index) => (
-                <div key={index}>
-                    <p>{task}</p>
-                </div>
-            ))}
+            <form onSubmit={handleSubmit}>
+                <ul>
+                    {tasks.map((t, index) => <li key={index}>{t}</li>)}
+                </ul>
+                {TODO_FIELDS.map((fields, index) => (
+                    <div key={index}>
+                        <label htmlFor={fields.id}>{fields.label} : </label>
+                        <input
+                            type={fields.type}
+                            id={fields.id}
+                            name={fields.name}
+                            placeholder={fields.placeholder}
+                            value={newTask}
+                            onChange={handleChange}
+                        />
+                    </div>
+                ))}
+                <button>Valider</button>
+            </form>
         </>
     )
 }
